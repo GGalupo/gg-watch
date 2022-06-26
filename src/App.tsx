e427a38d0e0af9fe6920d5ec94c0a535df8d9296
Gustavo Galupo <ggalupo2@hotmail.com>
2022-06-26 14:35:28 -0300

    Get lessons from GraphCMS

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,29 @@
+import { gql, useQuery } from "@apollo/client";
+
+const GET_LESSONS_QUERY = gql`
+  query {
+    lessons {
+      id
+      title
+    }
+  }
+`;
+
+interface Lesson {
+  id: string;
+  title: string;
+}
+
 const App = () => {
-  return <h1 className="font-bold text-5xl">Hello world</h1>;
+  const { data } = useQuery<{ lessons: Lesson[] }>(GET_LESSONS_QUERY);
+
+  return (
+    <ul>
+      {data?.lessons.map((lesson) => (
+        <li key={lesson.id}>{lesson.title}</li>
+      ))}
+    </ul>
+  );
 };
 
 export default App;
