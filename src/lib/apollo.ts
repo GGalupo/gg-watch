004548aab3c21d95b855285a7b3e2d118c872a1c
Gustavo Galupo <ggalupo2@hotmail.com>
2022-07-06 22:28:16 -0300

    Update Apollo client configs

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: "https://api-sa-east-1.graphcms.com/v2/cl4uh5m6e0z1y01t74y24g9ea/master",
+  uri: import.meta.env.VITE_API_URL,
+  headers: {
+    Authorization: `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`,
+  },
   cache: new InMemoryCache(),
 });
