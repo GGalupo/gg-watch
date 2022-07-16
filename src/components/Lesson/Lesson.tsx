4f6d058f239233dbd126806505abf956b365d88c
Gustavo Galupo <ggalupo2@hotmail.com>
2022-07-16 00:19:02 -0300

    Different styles for active lesson

--- src/components/Lesson/Lesson.tsx
+++ src/components/Lesson/Lesson.tsx
@@ -1,6 +1,7 @@
 import { CheckCircle, Lock } from "phosphor-react";
 import { isPast, format } from "date-fns";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import classNames from "classnames";
 
 interface LessonProps {
   title: string;
@@ -9,7 +10,15 @@ interface LessonProps {
   type: "live" | "class";
 }
 
-export const Lesson = ({ title, slug, availableAt, type }: LessonProps) => {
+export const Lesson = ({
+  title,
+  slug: lessonSlug,
+  availableAt,
+  type,
+}: LessonProps) => {
+  const { slug } = useParams<{ slug: string }>();
+  const isActiveLesson = slug === lessonSlug;
+
   const isLessonAvailable = isPast(availableAt);
   const availableDateFormatted = format(
     availableAt,
@@ -17,10 +26,17 @@ export const Lesson = ({ title, slug, availableAt, type }: LessonProps) => {
   );
 
   return (
-    <Link to={`/lessons/${slug}`} className="group">
+    <Link to={`/lessons/${lessonSlug}`} className="group">
       <time className="text-gray-300 text-sm">{availableDateFormatted}</time>
 
-      <div className="rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500">
+      <div
+        className={classNames(
+          "rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500",
+          {
+            "bg-green-500": isActiveLesson,
+          }
+        )}
+      >
         <header className="flex items-center justify-between">
           {isLessonAvailable ? (
             <span className="flex items-center gap-2 text-sm text-blue-500 font-medium">
