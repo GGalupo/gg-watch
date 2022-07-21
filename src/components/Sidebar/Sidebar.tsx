804c1150dc56fd2a36c321aacf48a94a28e9f83a
Gustavo Galupo <ggalupo2@hotmail.com>
2022-07-20 23:15:50 -0300

    Videos page responsiveness

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -5,7 +5,7 @@ export const Sidebar = () => {
   const { data } = useGetLessonsQuery();
 
   return (
-    <aside className="w-[22rem] bg-gray-700 p-6 border-l border-gray-600">
+    <aside className="hidden lg:block w-[22rem] bg-gray-700 p-6 border-l border-gray-600">
       <h3 className="font-bold text-2xl pb-6 mb-6 border-b border-gray-500">
         Lessons schedule
       </h3>
