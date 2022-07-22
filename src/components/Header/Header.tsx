65c067c732c57eb8b459592d4baabb69fa2be52f
Gustavo Galupo <ggalupo2@hotmail.com>
2022-07-21 23:12:44 -0300

    Add Menu on Header on mobile screens

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,18 @@
+import { List } from "phosphor-react";
+
 import { Logo } from "..";
 
-export const Header = () => {
+interface HeaderProps {
+  toggleMenu: () => void;
+}
+
+export const Header = ({ toggleMenu }: HeaderProps) => {
   return (
-    <header className="flex py-5 justify-center items-center bg-gray-700 border-b border-gray-600">
+    <header className="flex p-5 justify-between lg:justify-center items-center bg-gray-700 border-b border-gray-600">
       <Logo />
+      <button className="lg:hidden" onClick={toggleMenu}>
+        <List size={30} aria-label="open mobile menu" color="#81D8F7" />
+      </button>
     </header>
   );
 };
