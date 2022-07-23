45200499120196623e8ac8e474eb343d9ba87eda
Gustavo Galupo <ggalupo2@hotmail.com>
2022-07-23 00:37:50 -0300

    Show lessons text next to mobile menu

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -10,9 +10,12 @@ export const Header = ({ toggleMenu }: HeaderProps) => {
   return (
     <header className="flex p-5 justify-between lg:justify-center items-center bg-gray-700 border-b border-gray-600">
       <Logo />
-      <button className="lg:hidden" onClick={toggleMenu}>
-        <List size={30} aria-label="open mobile menu" color="#81D8F7" />
-      </button>
+      <div className="flex items-center gap-4 lg:hidden">
+        <span className="text-[0.6rem] font-medium text-blue-500">LESSONS</span>
+        <button onClick={toggleMenu}>
+          <List size={30} aria-label="open mobile menu" color="#81D8F7" />
+        </button>
+      </div>
     </header>
   );
 };
