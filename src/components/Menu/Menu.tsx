97ef20934b7d2e26bc961afa2271076dfa95f7ab
Gustavo Galupo <ggalupo2@hotmail.com>
2022-07-22 23:54:28 -0300

    Mobile menu header

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,5 +1,7 @@
 import { X } from "phosphor-react";
 
+import { Logo } from "../Logo";
+
 interface MenuProps {
   isMenuOpen: boolean;
   closeMenu: () => void;
@@ -8,13 +10,16 @@ interface MenuProps {
 export const Menu = ({ closeMenu, isMenuOpen }: MenuProps) => {
   return (
     <div
-      className={`w-full h-screen overflow-auto bg-gray-700 md:hidden z-50 ${
-        isMenuOpen ? "absolute" : "hidden"
+      className={`w-full h-screen overflow-y-auto bg-gray-700 lg:hidden backdrop-blur-sm z-[9999] ${
+        isMenuOpen ? "absolute inset-0" : "hidden"
       }`}
     >
-      <button onClick={closeMenu}>
-        <X size={30} color="#81D8F7" />
-      </button>
+      <header className="flex p-5 justify-between items-center bg-gray-700 border-b border-gray-600">
+        <Logo />
+        <button onClick={closeMenu}>
+          <X size={30} color="#81D8F7" />
+        </button>
+      </header>
     </div>
   );
 };
