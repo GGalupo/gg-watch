e40d64fd6fdd720d939ff6511e415020d2724776
Gustavo Galupo <ggalupo2@hotmail.com>
2022-07-23 23:22:30 -0300

    Add value prop to AuthContext provider

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,11 +1,11 @@
-import { createContext, useState, type ReactNode } from "react";
+import { createContext, useEffect, useState, type ReactNode } from "react";
 
 type User = {
   name: string;
 };
 
 interface AuthContextData {
-  user: User;
+  user: User | null;
   signInWithGitHub: () => void;
   signOut: () => void;
 }
@@ -19,5 +19,15 @@ export const AuthContext = createContext({} as AuthContextData);
 export const AuthContextProvider = ({ children }: AuthContextProviderProps) => {
   const [user, setUser] = useState<User | null>(null);
 
-  return <AuthContext.Provider value={{}}>{children}</AuthContext.Provider>;
+  useEffect(() => {}, []);
+
+  const signInWithGitHub = () => {};
+
+  const signOut = () => {};
+
+  return (
+    <AuthContext.Provider value={{ user, signInWithGitHub, signOut }}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
