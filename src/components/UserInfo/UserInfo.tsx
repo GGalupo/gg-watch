7e10b6ae2dcdf9c19d328bed750c48422779b0b3
Gustavo Galupo <ggalupo2@hotmail.com>
2022-07-29 01:48:05 -0300

    Add different styles to UserInfo component on lessons pages

--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -2,11 +2,19 @@ import { Link } from "react-router-dom";
 
 import { useAuth } from "../../hooks";
 
-export const UserInfo = () => {
+interface UserInfoProps {
+  isLessonsPages?: boolean;
+}
+
+export const UserInfo = ({ isLessonsPages }: UserInfoProps) => {
   const { user, signOut } = useAuth();
 
   return (
-    <div className="flex items-center gap-4">
+    <div
+      className={`items-center gap-4 ${
+        isLessonsPages ? "hidden lg:flex" : "flex"
+      }`}
+    >
       <img
         src={user?.imageURL}
         alt={user?.name}
@@ -15,16 +23,23 @@ export const UserInfo = () => {
       <div className="flex flex-col">
         <span className="font-bold">{user?.name}</span>
         <span className="text-sm">{user?.email}</span>
-        <div className="flex items-center gap-4 mt-2">
-          <Link
-            to="/lessons"
-            className="text-blue-500 font-medium hover:font-bold"
-          >
-            Go to lessons
-          </Link>
+
+        <div
+          className={`flex items-center gap-4 ${isLessonsPages ? "" : "mt-2"}`}
+        >
+          {!isLessonsPages && (
+            <Link
+              to="/lessons"
+              className="text-blue-500 font-medium hover:font-bold"
+            >
+              Go to lessons
+            </Link>
+          )}
           <button
             onClick={signOut}
-            className="text-red-400  font-medium hover:font-bold"
+            className={`text-red-400  font-medium hover:font-bold ${
+              isLessonsPages ? "self-start mt-1" : ""
+            }`}
           >
             Logout
           </button>
